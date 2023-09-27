0d5fa600b722ddd544c43a4e112e708dc669cf12
Abdulla <abdulgour29@gmail.com>
2023-09-27 22:25:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Form } from "./Components/Form";
+
+function App() {
+
+  return (
+    <div className="wrapper">
+      <h3 className="form-name">TODO APP</h3>
+      <div className="form-and-todo-box">
+        <Form/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
